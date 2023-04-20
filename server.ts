3a5e4c4b54e06b3acf564f2e9b9f70a6e440282e
Aditya Manikanth Rao <adityamanikanthrao@gmail.com>
2023-04-20 23:57:10 +0530

    updated port

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import { startStandaloneServer } from '@apollo/server/standalone';
 import chalk from 'chalk';
 
+import { env } from './src/utils/config/env';
 import { server } from './src/utils/config/server-config';
 import { db } from './src/utils/functions/db';
 import { Context } from './src/utils/types/context';
@@ -10,7 +11,7 @@ const start = async () => {
     context: async (context) => {
       return { ...context, db };
     },
-    listen: { port: 4000 },
+    listen: { port: +env.PORT },
   });
 
   console.log(chalk.hex('#f5587b')(`🚀 Server running at ${url}graphql`));
