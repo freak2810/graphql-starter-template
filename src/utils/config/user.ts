d9132040b900564248ca842a4ac23603fed4a429
Aditya Manikanth Rao <adityamanikanthrao@gmail.com>
2023-07-10 12:33:32 +0530

    added documentation

--- src/utils/config/user.ts
+++ src/utils/config/user.ts
@@ -2,6 +2,18 @@ import { parsePhoneNumber } from 'libphonenumber-js';
 
 import { LoggedInUser } from '../types/context';
 
+/**
+ * This function will parse the token passed from the Apollo Federation Gateway.
+ * A few points to take care of
+ * * The token is passed as a stringified JSON object
+ * * The token should be passed in the header as `x-user`
+ * * You can access the user as `context.user` in the resolvers.
+ * * You can customize the type of the user by changing {@link LoggedInUser}
+ *
+ * @param header The header passed from the Apollo Federation Gateway
+ * @returns Details of the logged in user, if any. Else, null.
+ *
+ */
 export const getUserFromHeader = (header: string | undefined | string[]) => {
   if (!header || Array.isArray(header)) return null;
 
